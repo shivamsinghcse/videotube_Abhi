fb3095cffb17b13a0e39cea69dbf5306b76d6fda
shivamsinghrajputcse <shivamsinghcse21@gmail.com>
2023-06-16 14:05:35 +0530

    fixing js code of darkmode

--- script.js
+++ script.js
@@ -4,33 +4,34 @@ let container = document.querySelector(".container");
 
 
 
-menuIcon.onclick = function(){
+menuIcon.onclick = function () {
     sidebar.classList.toggle("small-sidebar");
     container.classList.toggle("large-container");
 
-    
+
 }
+
 let mode = document.getElementById('mode');
 let link = document.querySelectorAll('a.navLeftLinks');
 let vidInfo = document.querySelectorAll('div.vid-info');
 let modebutton = document.getElementById('modebutton');
 // let modebuttonimg = document.getElementById('modebuttonimg');
-function modechanger(){
+function modechanger() {
     mode.style.color = 'white';
     mode.style.backgroundColor = 'black';
-    link.forEach(function(link) {
+    link.forEach(function (link) {
         link.style.color = 'white';
     });
-    document.getElementById('modebuttonimg').src= 'images/darkmode.svg';
+    document.getElementById('modebuttonimg').src = 'images/darkmode.svg';
     document.getElementById('modebutton').onclick = modechangerlight;
 }
-function modechangerlight(){
+function modechangerlight() {
     mode.style.color = 'black';
     mode.style.backgroundColor = 'white';
-    link.forEach(function(link) {
+    link.forEach(function (link) {
         link.style.color = 'black';
     });
-    document.getElementById('modebuttonimg').src= 'images/lightmode.svg';
+    document.getElementById('modebuttonimg').src = 'images/lightmode.svg';
     document.getElementById('modebutton').onclick = modechanger;
 }
 
